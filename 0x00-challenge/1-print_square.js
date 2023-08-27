8bd262930ed4291be01f2576169bf4534507dc3a
Mario Nageh <marionageh7@gmail.com>
2023-08-27 21:56:07 +0300

    Fix Js

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -13,12 +13,11 @@ if (process.argv.length <= 2) {
     process.stderr.write("Example: ./1-print_square.js 8\n");
     process.exit(1)
 }
-
-size = parseInt(process.argv[2], 16)
-
+//0111196777884
+size = parseInt(process.argv[2], 10)
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
         process.stdout.write("#");
     }
-    process.stdout.write("\n");
+    process.stdout.write(`\n`);
 }
